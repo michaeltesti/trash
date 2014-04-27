e697aa2357cf26d8758fc46a321ae94e9fecc18e
Michael Spielvogel <michael.spielvogel@gmx.at>
2014-04-27 16:49:06 +0200

    branch 3 coming from local Signed-off-by: Michael Spielvogel <michael.spielvogel@gmx.at>

---
+++ myProject/src/myProject/MyApp3.java
@@ -0,0 +1,10 @@
+package myProject;
+
+public class MyApp3 {
+	public static void main (String[] args)
+	{
+		System.out.println("Remote");
+		System.out.println("Remote2");
+		System.out.println("Branch7");
+	}
+}
