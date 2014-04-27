251e357645a6f31e9d40fb431c05c7bb99953132
Michael Spielvogel <michael.spielvogel@gmx.at>
2014-04-27 21:36:25 +0200

    d

--- myProject/src/myProject/MyApp3.java
+++ myProject/src/myProject/MyApp3.java
@@ -1,5 +1,14 @@
 package myProject;
 
 public class MyApp3 {
+<<<<<<< Upstream, based on origin/v2
 
+=======
+	public static void main (String[] args)
+	{
+		System.out.println("Remote");
+		System.out.println("Remote2");
+		System.out.println("Branch7");
+	}
+>>>>>>> e697aa2 branch 3 coming from local Signed-off-by: Michael Spielvogel <michael.spielvogel@gmx.at>
 }
