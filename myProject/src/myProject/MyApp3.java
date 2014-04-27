47a339c314da084ccca4fc013d5f14365e274693
Michael Spielvogel <michael.spielvogel@gmx.at>
2014-04-27 16:49:06 +0200

    comming from local Signed-off-by: Michael Spielvogel <michael.spielvogel@gmx.at>

---
+++ myProject/src/myProject/MyApp3.java
@@ -0,0 +1,9 @@
+package myProject;
+
+public class MyApp3 {
+	public static void main (String[] args)
+	{
+		System.out.println("Remote");
+		System.out.println("Remote2");
+	}
+}
--- myProject/src/myProject/myApp.java
+++ myProject/src/myProject/myApp.java
@@ -13,8 +13,10 @@ public class myApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world2");
 		System.out.println("Added git");
+		System.out.println("Fall back git");
+		System.out.println("Next git24");
 	}
 
 }
