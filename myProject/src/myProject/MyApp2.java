c991a278669a4982c4a16421c2b3866a68c8f406
Michael Spielvogel <michael.spielvogel@gmx.at>
2014-04-27 16:30:20 +0200

    Second tried out single ressource change
    Signed-off-by: Michael Spielvogel <michael.spielvogel@gmx.at>

---
+++ myProject/src/myProject/MyApp2.java
@@ -0,0 +1,8 @@
+package myProject;
+
+public class MyApp2 {
+	public static void main (String[] args)
+	{
+		System.out.println("Hello World 2");
+	}
+}
---
+++ myProject/src/myProject/myApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myProject;
+
+/**
+ * @author Spieli
+ *
+ */
+public class myApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Added git");
+	}
+
+}
